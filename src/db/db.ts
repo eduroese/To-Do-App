93748f316685fddc79e243df0ba2406eb23da2b9
eduroese <dudusantos58@hotmail.com>
2024-12-31 20:16:14 -0300

    Connecting project to Mongo Database

---
+++ src/db/db.ts
@@ -0,0 +1,22 @@
+import { connect } from "http2";
+
+const mongoose = require('mongoose');
+
+// Connection String
+const uri = 'mongodb://localhost:27017/myDatabase';
+
+// Conectar ao MongoDB com Mongoose
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Conectado ao MongoDB com sucesso!');
+  } catch (error) {
+    console.error('Erro ao conectar ao MongoDB:', error);
+    throw error;
+  }
+}
+
+export default connectToDatabase;
